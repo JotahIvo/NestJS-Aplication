a79db59647074bb88024b006e7296bbe99ebd5d6
Joao Pedro de Almeida Ivo <jota@MacBook-Air-de-Joao.local>
2025-09-02 13:19:07 -0300

    fix: disable user validation

--- src/user/dtos/createUser.dto.ts
+++ src/user/dtos/createUser.dto.ts
@@ -1,16 +1,8 @@
 /* eslint-disable @typescript-eslint/no-unsafe-call */
-import { IsAlpha, IsEmail, IsNotEmpty, IsString } from 'class-validator';
-
 export class CreateUserDto {
-  @IsEmail()
-  @IsNotEmpty()
   email: string;
 
-  @IsString()
-  @IsNotEmpty()
   name: string;
 
-  @IsAlpha()
-  @IsNotEmpty()
   password: string;
 }

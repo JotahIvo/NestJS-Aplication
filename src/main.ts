64b22782fbd24cf6781e1c4e01e2ff092d18ff39
JotahIvo <j.pedro1415@ufu.br>
2025-09-07 12:00:33 -0300

    refactor(core): Implement a graceful shutdown mechanism

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,11 @@ import { Logger } from 'nestjs-pino';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bufferLogs: true });
-  app.useLogger(app.get(Logger)); 
+  app.useLogger(app.get(Logger));
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new PrismaClientExceptionFilter());
+
+  app.enableShutdownHooks();
 
   const port = process.env.PORT ?? 3000;
   await app.listen(port);

7515e069d8531fa4c3ff46531537d1dcf2add51b
JotahIvo <j.pedro1415@ufu.br>
2025-09-07 11:05:43 -0300

    feat: Add validation decorators to CreateUserDto

--- src/user/dtos/createUser.dto.ts
+++ src/user/dtos/createUser.dto.ts
@@ -1,8 +1,16 @@
-/* eslint-disable @typescript-eslint/no-unsafe-call */
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+
 export class CreateUserDto {
+  @IsEmail()
+  @IsNotEmpty()
   email: string;
 
+  @IsString()
+  @IsNotEmpty()
   name: string;
 
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(8, { message: 'Password must be at least 8 characters long' })
   password: string;
 }
